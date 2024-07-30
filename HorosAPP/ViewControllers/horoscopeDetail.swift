28798c0cb72686ad2c7b7538de562232cfa6c38b
RubenNeo <ruben.lazaronp@gmail.com>
2024-07-30 12:54:07 +0200

    Detail Prepared

---
+++ HorosAPP/ViewControllers/horoscopeDetail.swift
@@ -0,0 +1,36 @@
+//
+//  horoscopeDetail.swift
+//  HorosAPP
+//
+//  Created by Mañanas on 30/7/24.
+//
+
+import UIKit
+
+class horoscopeDetail: UIViewController {
+    
+    @IBOutlet weak var logoImageView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var datesLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
